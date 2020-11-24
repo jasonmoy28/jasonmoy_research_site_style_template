c01594cbc28ccb7663ca3d4bd5236e6019894413
Jason Moy <jasonmoy28@gmail.com>
2020-11-25 00:13:08 +0800

    type: <feat> action: <allowed up to 10 authors (dynamic change)>

--- javascript/main.js
+++ javascript/main.js
@@ -7,10 +7,97 @@ document.querySelector("#footer").textContent = global.footer;
 try {
   document.getElementById("abstract").innerHTML = indexHTML.abstract;
   document.getElementById("mainTitle").innerHTML = indexHTML.main_title;
-  document.getElementById("author1").innerText = indexHTML.author1;
-  document.getElementById("author1").href = indexHTML.author1_link;
-  document.getElementById("author2").innerHTML = indexHTML.author2;
-  document.getElementById("author2").href = indexHTML.author2_link;
+  // Added author name if the name is not null. Add href to associated author link, if null, add # to the link
+  const author1 = indexHTML.author1;
+  const author2 = indexHTML.author2;
+  const author3 = indexHTML.author3;
+  const author4 = indexHTML.author4;
+  const author5 = indexHTML.author5;
+  const author6 = indexHTML.author6;
+  const author7 = indexHTML.author7;
+  const author8 = indexHTML.author8;
+  const author9 = indexHTML.author9;
+  const author10 = indexHTML.author10;
+
+  const authors_name = [
+    author1,
+    author2,
+    author3,
+    author4,
+    author5,
+    author6,
+    author7,
+    author8,
+    author9,
+    author10,
+  ];
+  // Use a for loop to create a-tags and stored them as an array
+  let elements = [];
+  for (let index = 0; index < authors_name.length; index++) {
+    let authorElement = document.createElement("a");
+    authorElement.innerText = authors_name[index].name;
+    authorElement.href = authors_name[index].link;
+    authorElement.target = "_blank";
+    elements.push(authorElement);
+  }
+  // Get the element
+  const authors = document.querySelector("#authors");
+  authors.appendChild(elements[0]);
+
+  // #Use if_else statement to add a-tag dynamically depends on the name null or not
+  // * Two authors has a special case
+  // Set up the condition for only author 1 & author 2 special case
+  let conditionCheck = [];
+  for (let i = 2; i < 10; i++) {
+    authors.classList.add("two-author");
+    let conditionCheckInLoop = authors_name[i].name;
+    conditionCheck.push(conditionCheckInLoop);
+  }
+
+  if (
+    author2.name != null &&
+    conditionCheck.every((isNull) => isNull === null)
+  ) {
+    authors.append(" & ");
+    authors.appendChild(elements[1]);
+  } else if (author2.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[1]);
+  }
+  // Other if_else statment
+  if (author3.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[2]);
+  }
+  if (author4.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[3]);
+  }
+  if (author5.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[4]);
+  }
+  if (author6.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[5]);
+  }
+  if (author7.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[6]);
+  }
+  if (author8.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[7]);
+  }
+  if (author9.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[8]);
+  }
+  if (author10.name != null) {
+    authors.append(" , ");
+    authors.appendChild(elements[9]);
+  }
+
   document.getElementById("affiliationName").innerText =
     indexHTML.affiliation_name;
   document.getElementById("labLink").innerHTML = indexHTML.lab_name;
@@ -18,7 +105,7 @@ try {
   document.getElementById("github-logo").href = indexHTML.github_link;
   document.getElementById("github-text").href = indexHTML.github_link;
 } catch (error) {
-  console.log("Not on index.html");
+  console.log(error);
 }
 
 // #manuscript.html content
@@ -28,7 +115,7 @@ try {
   document.getElementById("manuscript-iframe-source").src =
     manuscript.manuscript_source;
 } catch (error) {
-  console.log("Not on manuscript.html");
+  console.log("Not in manuscript.html");
 }
 
 // #visualization.html content
@@ -46,7 +133,7 @@ try {
   document.querySelectorAll(".img-container img")[5].src =
     visualization.img6_src;
 } catch (error) {
-  console.log("Not on visualization.html");
+  console.log("Not in visualization.html");
 }
 
 // #poster.html content
@@ -54,5 +141,5 @@ try {
   document.getElementById("poster-download-source").href = poster.poster_source;
   document.getElementById("poster-iframe-source").src = poster.poster_source;
 } catch (error) {
-  console.log("Not on poster.html");
+  console.log("Not in poster.html");
 }
